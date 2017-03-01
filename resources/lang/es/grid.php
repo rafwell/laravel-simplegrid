bb4ff182779e9aa96a223253662bd249200061ca
Rafael Alves <rafaotec@gmail.com>
2017-03-01 17:35:46 -0300

    Spanish language

---
+++ resources/lang/es/grid.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+	'Advanced Search'=>'Búsqueda Avanzada',
+	'Search'=>'Buscar',
+	'Simple Search'=>'Búsqueda simple',
+	'Page :current_page of :total_pages. Total of :total_rows row.'=>'Página :current_page de :total_pages. Total de :total_rows fila.|Página :current_page de :total_pages. Total de :total_rows filas.',
+	'Search by...'=>'Búsqueda por...',
+	'Clear search'=>'Borrar búsqueda',
+	'Apply to selected'=>'Aplicar a los seleccionados',
+	'Showing'=>'Demostración',
+	'rows per page.'=>'filas por página.',
+	'Order ascending'=>'Orden ascendente',
+	'Order descending'=>'Orden decreciente',
+	'Actions'=>'Acciones',
+	'No results found.'=>'No se han encontrado resultados.',
+	'Select an option to export'=>'Seleccione una opción para exportar',
+	'Select a format for export!'=>'Seleccione un formato para exportar!',
+	'Export'=>'Exportar',
+	'Previous Page'=>'Página anterior',
+	'Next Page'=>'Siguiente página',
+	'Page'=>'Página',
+	'from:'=>'de:',
+	'to:'=>'a:',
+	'Do you really want to apply this action to the selected items?'=>'¿Realmente desea aplicar esta acción a los elementos seleccionados?'
+];
