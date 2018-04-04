a11687382e75ccb2ef500b8b7646041d23dac0c2
Rafael <rafaotec@gmail.com>
2018-04-04 11:03:25 -0300

    Fix datetime format in config file

--- config/rafwell-simplegrid.php
+++ config/rafwell-simplegrid.php
@@ -19,11 +19,11 @@ return [
 				//allow translate the date visual format to backend format. the index 0 is the js format (moment) and the index 1 is the php format (carbon)
 				/*
 					Example for Brazil standard					
-					'input'=>['DD/MM/YYYY HH:MM:ss', 'd/m/Y H:i:s'], 
-					'processTo'=>['YYYY-MM-DD HH:MM:ss', 'Y-m-d H:i:s']
+					'input'=>['DD/MM/YYYY HH:mm:ss', 'd/m/Y H:i:s'], 
+					'processTo'=>['YYYY-MM-DD HH:mm:ss', 'Y-m-d H:i:s']
 				*/
-				'input'=>['YYYY-MM-DD HH:MM:ss', 'd/m/Y H:i:s'], 
-				'processTo'=>['YYYY-MM-DD HH:MM:ss', 'Y-m-d H:i:s']
+				'input'=>['YYYY-MM-DD HH:mm:ss', 'd/m/Y H:i:s'], 
+				'processTo'=>['YYYY-MM-DD HH:mm:ss', 'Y-m-d H:i:s']
 			]
 		]
 	]
