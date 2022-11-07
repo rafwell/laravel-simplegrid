e2cbb5480883a8d544017f240f81b13371ba1105
Rafael Souza <rafaotec@gmail.com>
2022-11-07 17:04:44 -0300

    Add pt language

---
+++ resources/lang/pt/grid.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'Advanced Search' => 'Pesquisa Avançada',
+    'Search' => 'Pesquisar',
+    'Simple Search' => 'Pesquisa Simples',
+    'Page :current_page of :total_pages. Total of :total_rows row.' => 'Página :current_page de :total_pages. Total de :total_rows registo.|Página :current_page de :total_pages. Total de :total_rows registos.',
+    'Search by...' => 'Pesquisar por...',
+    'Clear search' => 'Limpar pesquisa',
+    'Apply to selected' => 'Aplicar aos selecionados',
+    'Showing' => 'Exibindo',
+    'rows per page.' => 'registros por página.',
+    'Order ascending' => 'Ordenar crescente',
+    'Order descending' => 'Ordenar decrescente',
+    'Actions' => 'Ações',
+    'No results found.' => 'Nenhum resultado encontrado.',
+    'Do a search to get data.' => 'Faça uma pesquisa para obter dados.',
+    'Select an option to export' => 'Selecione uma opção para exportar',
+    'Select a format for export!' => 'Selecione um formato para exportar!',
+    'Export' => 'Exportar',
+    'Previous Page' => 'Página anterior',
+    'Next Page' => 'Próxima Página',
+    'Page' => 'Página',
+    'from:' => 'de:',
+    'to:' => 'até:',
+    'Do you really want to apply this action to the selected items?' => 'Deseja realmente aplicar esta ação aos itens selecionados?',
+    'Select at least one item to apply the action!' => 'Selecione ao menos um item para aplicar a ação!'
+];
